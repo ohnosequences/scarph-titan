b263a8ba1db6e52b1199ee3f2d1f19b5ca3dfed3
Alexey Alekhin <laughedelic@gmail.com>
2015-04-15 19:25:54 +0200

    Added property implementations

--- build.sbt
+++ build.sbt
@@ -16,7 +16,7 @@ libraryDependencies ++= Seq(
 //  },
   "ohnosequences"           %% "cosas"       % "0.6.0",
   "ohnosequences"           %% "scarph"      % "0.5.0.new-syntax-SNAPSHOT",
-  "com.thinkaurelius.titan" %  "titan-core"  % "0.5.3",
+  "com.thinkaurelius.titan" %  "titan-core"  % "0.5.4",
   "org.scalatest"           %% "scalatest"   % "2.2.4" % Test,
   "org.slf4j"               %  "slf4j-nop"   % "1.7.5" % Test
   // ^ getting rid of the annoying warning about logging ^
--- src/main/scala/ohnosequences/scarph/impl/titan/implementations.scala
+++ src/main/scala/ohnosequences/scarph/impl/titan/implementations.scala
@@ -4,7 +4,7 @@ case object implementations {
 
   import com.thinkaurelius.titan.{ core => titan } //, schema._
   import scala.collection.JavaConversions._
-  import java.lang.{ Iterable => JIterable }
+  // import java.lang.Iterable
 
   import ohnosequences.cosas._, fns._, types._
   // import cosas.ops.typeSets._
@@ -13,39 +13,62 @@ case object implementations {
   import s.graphTypes._, s.morphisms._, s.implementations._
   // import s.impl.titan.predicates._
 
-  trait AnyTitanElement {
+  case class TitanImpls(val graph: titan.TitanGraph) {
 
-    val graph: titan.TitanGraph
+    type Container[T] = java.lang.Iterable[T]
 
-    type Obj
-    val values: JIterable[Obj]
-  }
+    trait AnyTitanVals {
 
-  implicit def unitImpl[O <: AnyTitanElement]:
-      UnitImpl[titan.TitanGraph, O] =
-  new UnitImpl[titan.TitanGraph, O] {
-
-    def toUnit(o: Obj): UnitImpl = o.graph
-    // TODO: return all graph elements of this type
-    def fromUnit(u: UnitImpl): Obj = ???
-  }
-
-
-  trait AnyTitanVertexImpl extends AnyTitanElement {
-
-    type Obj = titan.TitanVertex
-  }
-
-
-  implicit def propertyImpl[P <: AnyGraphProperty]:
-      PropertyImpl[P#Raw, titan.TitanElement] =
-  new PropertyImpl[P#Raw, titan.TitanElement] {
-
-    def get(e: Element, p: P): Property = {
-      e.getProperty[p.Raw](p.label)
+      type Obj
+      val values: Container[Obj]
     }
 
-    def lookup(p: Property): Element = ???
+    case class TitanVals[T](val values: Container[T])
+      extends AnyTitanVals { type Obj = T }
+
+    type TitanVertices = TitanVals[titan.TitanVertex]
+    type TitanEdges = TitanVals[titan.TitanEdge]
+
+    // TODO: should work also for properties and predicates
+    implicit def unitVertexImpl[V <: AnyVertex, O <: AnyTitanVals]:
+        UnitImpl[V, O, titan.TitanGraph] =
+    new UnitImpl[V, O, titan.TitanGraph] {
+
+      def toUnit(o: RawObject): RawUnit = graph
+      // TODO: return all vertices of this type
+      def fromUnit(u: RawUnit, o: Object): RawObject = ???
+    }
+
+
+
+    implicit def vertexPropertyImpl[P <: AnyGraphProperty]:
+        PropertyImpl[P, TitanVals[titan.TitanVertex], TitanVals[P#Raw]] =
+    new PropertyImpl[P, TitanVals[titan.TitanVertex], TitanVals[P#Raw]] {
+
+      def get(e: RawElement, p: Property): RawValue = TitanVals(asJavaIterable(
+        e.values.map{ _.getProperty[P#Raw](p.label) }
+      ))
+
+      def lookup(r: RawValue, p: Property): RawElement = TitanVals(r.values.map { v =>
+        graph.query.has(p.label, v).vertices
+      }.asInstanceOf[Container[titan.TitanVertex]])
+    }
+
+    implicit def edgePropertyImpl[P <: AnyGraphProperty]:
+        PropertyImpl[P, TitanEdges, TitanVals[P#Raw]] =
+    new PropertyImpl[P, TitanEdges, TitanVals[P#Raw]] {
+
+      def get(e: RawElement, p: Property): RawValue = TitanVals(asJavaIterable(
+        e.values.map{ _.getProperty[P#Raw](p.label) }
+      ))
+
+      def lookup(r: RawValue, p: Property): RawElement = TitanVals(
+        r.values.flatMap { v =>
+          graph.query.has(p.label, v).vertices
+        }.asInstanceOf[Container[titan.TitanEdge]]
+      )
+    }
+
   }
 
 }

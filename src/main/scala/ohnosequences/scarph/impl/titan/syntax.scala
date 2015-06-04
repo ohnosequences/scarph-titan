ec57a33032803d6d7d4bf133b74e24df0636e40b
Alexey Alekhin <laughedelic@gmail.com>
2015-06-04 20:14:16 +0200

    Mirrored dummy tests structure; added syntax for tagged vals

---
+++ src/main/scala/ohnosequences/scarph/impl/titan/syntax.scala
@@ -0,0 +1,21 @@
+package ohnosequences.scarph.impl.titan
+
+case object syntax {
+
+  import types._
+  import ohnosequences.cosas.types._
+  import ohnosequences.scarph.objects._
+
+  implicit def titanObjectValSyntax[F <: AnyGraphObject, VF <: AnyTitanType](vf: F := VF):
+    TitanObjectValSyntax[F, VF] =
+    TitanObjectValSyntax[F, VF](vf.value)
+
+  case class TitanObjectValSyntax[F <: AnyGraphObject, VF <: AnyTitanType](vf: VF) extends AnyVal {
+
+    def ⊗[S <: AnyGraphObject, VS <: AnyTitanType](vs: S := VS): (F ⊗ S) := Duplet[VF, VS] =
+      new Denotes( Duplet(vf, vs.value) )
+
+    def ⊕[S <: AnyGraphObject, VS <: AnyTitanType](vs: S := VS): (F ⊕ S) := Duplet[VF, VS] =
+      new Denotes( Duplet(vf, vs.value) )
+  }
+}
--- src/test/scala/ohnosequences/scarph/titan/schemaTests.scala
+++ src/test/scala/ohnosequences/scarph/titan/schemaTests.scala
@@ -1,110 +1,123 @@
 package ohnosequences.scarph.impl.titan.test
 
-import ohnosequences.scarph._, objects._
-import com.thinkaurelius.titan.{ core => titan }
-//import scala.reflect.runtime.universe._*/
+import com.thinkaurelius.titan.core
 
 
 class TitanSuite extends org.scalatest.FunSuite with org.scalatest.BeforeAndAfterAll {
 
-  import ohnosequences.scarph._, evals._, morphisms._, rewrites._
-  import syntax.objects._, syntax.morphisms._
-  import ohnosequences.scarph.impl.titan.evals._
-  import ohnosequences.scarph.impl.titan.types._
-  import ohnosequences.scarph.impl.titan.rewrites._
+  import ohnosequences.{ scarph => s }
+  import s.objects._, s.evals._, s.morphisms._
+  import s.syntax.objects._, s.syntax.morphisms._
+  import s.test._, twitter._
 
-  val impl = ohnosequences.scarph.impl.titan.evals.all(null: titan.TitanGraph); import impl._
-
-  test("eval basic queries over sample twitter graph") {
-
-    import ohnosequences.scarph.test._, twitter._
-
-    val query1 =
-      lookup(user.name) >=>
-      inV(follows) >=>
-      quantify(user ? (user.age > 10)) >=>
-      coerce(user ? (user.age > 10)) >=>
-      fork(user) >=>
-      (outV(follows) ⊕ inV(follows)) >=>
-      (outV(posted) ⊕ outV(posted)) >=>
-      merge(tweet) >=>
-      get(tweet.text)
-
-    val query2 =
-      lookup(user.name)
-      .inV(follows)
-      .filter(user ? (user.age > 10))
-      .fork
-      .andThen(outV(follows) ⊕ inV(follows))
-      .andThen(outV(posted) ⊕ outV(posted))
-      .merge
-      .get(tweet.text)
-
-    assert(query1 == query2)
-
-    val q =
-      id(user)
-      .outV(follows)
-      .outE(posted)
-      .quantify(posted ? (posted.time =/= ""))
-
-    println("\n----------------")
-    println("rewritten query:")
-    println((apply(edgeQuantification) to q).label)
-
-    println(evalOn[TitanVertices](q).evalPlan)
-
-    //println(evalOn[Container[String]](query2).evalPlan)*/
-
-    //lazy val z = evaluate(query) on (
-    //  name := Seq("@laughedelic", "@eparejatobes", "@evdokim")
-    //)
-
-    println("\n----------------")
-    println("results:")
-    //z.value.foreach(println)*/
-
-    println("\n----------------")
-  }
-
+  import ohnosequences.scarph.impl.{ titan => t }
+  import t.evals._, t.types._, t.rewrites._, t.syntax._
 
   import java.io.File
-  import com.thinkaurelius.titan.core._
 
-  val graphLocation = new File("/tmp/titanTest")
-  var g: titan.TitanGraph = null
+  // val graphLocation = new File("/tmp/titanTest")
+  // var twitterGraph: titan.TitanGraph = null
+  val twitterGraph = core.TitanFactory.open("inmemory")
 
-  /*override final def beforeAll() {
+  override final def beforeAll() {
 
+    /*
     def cleanDir(f: File) {
       if (f.isDirectory) f.listFiles.foreach(cleanDir(_))
       else { println(f.toString); f.delete }
     }
     cleanDir(graphLocation)
-    g = TitanFactory.open("berkeleyje:" + graphLocation.getAbsolutePath)
+    */
+
+    // twitterGraph = TitanFactory.open("berkeleyje:" + graphLocation.getAbsolutePath)
 
     println("Created Titan graph")
-
 
     import ohnosequences.scarph.impl.titan.titanSchema._
     import ohnosequences.scarph.test._
 
-    g.createSchema(twitter)
+    // twitterGraph.createSchema(twitter)
 
     import com.tinkerpop.blueprints.util.io.graphson._
-    GraphSONReader.inputGraph(g, this.getClass.getResource("/twitter_graph.json").getPath)
+    GraphSONReader.inputGraph(twitterGraph, this.getClass.getResource("/twitter_graph.json").getPath)
 
-    println("loaded sample Twitter data")
+    println("Loaded sample Twitter data")
   }
 
   override final def afterAll() {
-    if(g != null) {
-      // NOTE: uncommend if you want to add data to the GraphSON:
-      // import com.tinkerpop.blueprints.util.io.graphson._
-      // GraphSONWriter.outputGraph(g, "graph_compact.json", GraphSONMode.COMPACT)
+    // NOTE: uncommend if you want to add data to the GraphSON:
+    // import com.tinkerpop.blueprints.util.io.graphson._
+    // GraphSONWriter.outputGraph(twitterGraph, "graph_compact.json", GraphSONMode.COMPACT)
 
-      g.shutdown
-      println("Shutdown Titan graph")
-    }
-  }*/
+    twitterGraph.shutdown
+    println("Shutdown Titan graph")
+  }
+
+  //val impl = ohnosequences.scarph.impl.titan.evals.all(twitterGraph); import impl._*/
+
+  val nouser = user := Container[core.TitanVertex](Seq())
+
+  test("checking evals for the basic structure") {
+    import t.evals.categoryStructure._
+    import queries.categoryStructure._
+
+    assert{ eval(q_id)(nouser) == nouser }
+    assert{ eval(q_comp1)(nouser) == nouser }
+    assert{ eval(q_comp2)(nouser) == nouser }
+  }
+
+  test("checking evals for the tensor structure") {
+    import t.evals.categoryStructure._
+    val ts = t.evals.tensorStructure(twitterGraph); import ts._
+    import queries.tensorStructure._
+
+    assert{ eval(q_tensor)(nouser ⊗ nouser ⊗ nouser) == nouser ⊗ nouser ⊗ nouser }
+    assert{ eval(q_dupl)(nouser ⊗ nouser) == nouser ⊗ nouser ⊗ nouser }
+    assert{ eval(q_match)(nouser ⊗ nouser) == nouser }
+    assert{ eval(q_comp)(nouser ⊗ nouser) == nouser }
+  }
+
+/*
+  test("checking evals for the biproduct structure") {
+    import t.evals.categoryStructure._
+    import t.evals.biproductStructure._
+    import queries.biproductStructure._
+
+    assert{ eval(q_inj)(dt) == du ⊕ du ⊕ dt }
+    assert{ eval(q_bip)(du ⊕ du ⊕ dt) == du ⊕ du ⊕ dt }
+    assert{ eval(q_fork)(du ⊕ dt) == du ⊕ du ⊕ dt }
+    assert{ eval(q_merge)(du ⊕ du) == du }
+    assert{ eval(q_comp)(du ⊕ dt) == dt }
+  }
+
+  test("checking evals for the graph structure") {
+    import t.evals.categoryStructure._
+    import t.evals.graphStructure._
+    import queries.graphStructure._
+
+    assert{ eval(q_outV)(du) == dt }
+    assert{ eval(q_inV)(dt) == du }
+    assert{ eval(q_compV)(du) == du }
+
+    assert{ eval(q_outE)(du) == dt }
+    assert{ eval(q_inE)(dt) == du }
+    assert{ eval(q_compE)(du) == du }
+  }
+
+  test("checking evals for the property structure") {
+    import t.evals.categoryStructure._
+    import t.evals.propertyStructure._
+    import queries.propertyStructure._
+
+    // FIXME: this works if you put dnames on the right (no tag/type parameter check)
+    assert{ eval(q_getV)(du) == dages }
+    assert{ eval(q_lookupV)(dnames) == du }
+    assert{ eval(q_compV)(dnames) == dages }
+
+    assert{ eval(q_getE)(dp) == dtimes }
+    assert{ eval(q_lookupE)(dtimes) == dp }
+    assert{ eval(q_compE)(dp) == dp }
+  }
+*/
+
 }
